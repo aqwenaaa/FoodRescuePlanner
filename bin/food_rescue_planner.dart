4921f3d886d4955963adf1283999eeb63fa0683f
aqwenaaa <aqueenahapsari22@smk.belajar.id>
2025-09-24 19:12:50 +0700

    initial commit

---
+++ bin/food_rescue_planner.dart
@@ -0,0 +1,5 @@
+import 'package:food_rescue_planner/food_rescue_planner.dart' as food_rescue_planner;
+
+void main(List<String> arguments) {
+  print('Hello world: ${food_rescue_planner.calculate()}!');
+}
---
+++ test/food_rescue_planner_test.dart
@@ -0,0 +1,8 @@
+import 'package:food_rescue_planner/food_rescue_planner.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
